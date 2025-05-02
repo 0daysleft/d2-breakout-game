cd5eed3fee4d355d9c2c7237e88ed322b32ad358
odaysleft <vincentarasa254@gmail.com>
2025-05-02 18:29:47 +0300

    adding all the position/angles of a block for manipulation

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ const blockHeight = 20
 
 class Block{
      constructor(xAxis, yAxis){
-
+          this.bottomLeft = [xAxis, yAxis]
+          this.bottomRight = [xAxis + blockWidth, yAxis]
+          this.topLeft = [xAxis, yAxis + blockHeight]
+          this.topRight = [xAxis + blockWidth. yAxis + blockHeight]
      }
 }
 
