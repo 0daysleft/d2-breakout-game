9943625f1b3de2043fcf985a1705a512285e391b
odaysleft <vincentarasa254@gmail.com>
2025-05-02 18:33:52 +0300

    for loop for going through the array

--- script.js
+++ script.js
@@ -14,14 +14,20 @@ class Block{
 
 const blocks = [
      new Block[10, 270]
+
 ]
 
 function addBlock(){
-     const block = document.createElement('div');
-     block.classList.add('block');
-     block.style.left = '50px'
-     block.style.bottom = '50px'
-     grid.appendChild(block)
+ 
+     for(let i = 0; i < block.length; i++){
+              const block = document.createElement('div');
+               block.classList.add('block');
+               block.style.left = block[i].bottomLeft[0]
+               block.style.bottom = block[i].bottomLeft[1]
+               grid.appendChild(block)
+
+     }
+
 }
 
 
