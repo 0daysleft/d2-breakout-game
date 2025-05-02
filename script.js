4b2daf9009bdeedbc84a6511e6e657d1b3221b37
odaysleft <vincentarasa254@gmail.com>
2025-05-02 18:12:11 +0300

    created a  div element

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 const grid = document.querySelector('.grid');
 
-const block = document.querySelector('.block')
+const block = document.createElement('div');
 
-block.classList.add('block')
+block.classList.add('block');
+grid.computedStyleMap.left = '50px'
+grid.appendChild('block')
