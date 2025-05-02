3f8df2a5a34b0cea0959df84c51a132974252005
odaysleft <vincentarasa254@gmail.com>
2025-05-02 19:06:04 +0300

    fixied tpe errors

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 
 const grid = document.querySelector('.grid');
 const blockWidth  = 100;
-const blockHeight = 20
+const blockHeight = 20;
 const userStart = [230, 10]
+let currentPosition = userStart;
 
 class Block{
      constructor(xAxis, yAxis){
@@ -56,5 +57,6 @@ addBlock()
 
 const user = document.createElement('div')
 user.classList.add('user')
-user
+user.style.left = currentPosition[0] + 'px'
+user.style.bottom = currentPosition[1] + 'px'
 grid.appendChild(user)
