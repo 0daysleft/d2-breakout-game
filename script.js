fee4a90ddb09ad71d0efc2b8731d72a1666ebda4
odaysleft <vincentarasa254@gmail.com>
2025-05-03 21:37:16 +0300

    checking for block collissions and selecting all the blocks

--- script.js
+++ script.js
@@ -122,6 +122,18 @@ function moveBall(){
 timerId = setInterval(moveBall, 30)
 
 function checkForCollision(){
+
+     //check for block collission
+     for(let i = 0; i < blocks.length; i++){
+          if(
+               (ballCurrentPosition[0] > blocks[i].bottomLeft[0] && ballCurrentPosition[0] < blocks[i].bottomRight[0]) && (ballCurrentPosition[1] + ballDiameter) > blocks[i].bottomLeft[1] && ballCurrentPosition[1] < blocks[i].topLeft[1]
+          ){
+               const allBlocks = Array.from(document.querySelectorAll('.block'))
+          }
+     }
+
+
+
      if(ballCurrentPosition[0] >= (boardWidth - ballDiameter) || ballCurrentPosition[1] >= (boardHeight - ballDiameter) || ballCurrentPosition[0] <= 0){
           changeDirection()
      }
