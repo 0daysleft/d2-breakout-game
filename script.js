084f5c26091d25de9c28f56e42ffe133f8cab83e
odaysleft <vincentarasa254@gmail.com>
2025-05-03 21:13:17 +0300

    adding a break point of ball not to collide with the wall

--- script.js
+++ script.js
@@ -121,7 +121,7 @@ function moveBall(){
 timerId = setInterval(moveBall, 30)
 
 function checkForCollision(){
-     if(ballCurrentPosition[0] >= (boardWidth - ballDiameter)){
+     if(ballCurrentPosition[0] >= (boardWidth - ballDiameter) || ballCurrentPosition[1] >= (boardHeight - ballDiameter) ){
           changeDirection()
      }
 }
